00b0fbf76e15165677159231c8181ba556450c87
varmer <75427804+varmer@users.noreply.github.com>
2021-03-05 18:18:58 +0000

    Add files via upload

--- Fg_codon_optimsation.py
+++ Fg_codon_optimsation.py
@@ -16,6 +16,11 @@ import re
 import numpy as np
 
 
+#### PART 1 
+# Determining species codon usage 
+
+
+
 # import coding sequences of fusarium 
 ### change so that this is a file path selector before uploading to github
 Species_cds = open(r"C:\Users\armerv\Desktop\BLAST\Genome_cds_fastas\Fusarium_graminearum_cds.fasta")
@@ -35,11 +40,6 @@ def get_coding_seqs(cds):
     return seqs
 
 
-# truncation check
-trunc_check = get_coding_seqs(Species_cds)
-#print(trunc_check)
-
-
 # convert DNA to mRNA in truncated coding sequences
 
 def DNA_to_mRNA(seqs):
@@ -48,32 +48,20 @@ def DNA_to_mRNA(seqs):
         sequences.append(cds.upper().replace('T', 'U'))    
     return sequences
 
-# mRNA check
-
-mRNA_check = DNA_to_mRNA(trunc_check)
-#print(mRNA_check)
-
-
 
 # split coding seqs into codons (all start at start codon already, but remove any that aren't factor of 3)
 
-def encode_codon_segments(mRNA_seq):
+def codon_segmentation(sequences):
         n = 3
         segments = []
-        for seq in mRNA_seq:
+        for seq in sequences:
             for i in range(0, len(seq), n):
                 segments.append(seq[i:i+n])
         return segments
 
-# segmentation check
-seg_check = encode_codon_segments(mRNA_check)
-#print(seg_check)
-
-
-
 # create a dictionary of amino acid codons
 ##break up 6-codon family into 2 and 4 fold (Ser (S), L(Leu), R (Arg))
-codon_to_aa = {
+c_to_aa = {
     "UUU":"Phe", "UUC":"Phe",         
     "UCU":"Ser4", "UCC":"Ser4", "UCA":"Ser4", "UCG":"Ser4",
     "AGU":"Ser2", "AGC":"Ser2",
@@ -102,30 +90,26 @@ codon_to_aa = {
 # returns dataframe of absolute codon frequencies
 
 
-def counter(codons):
+def counter(segments):
     codon_count = dict()
     
-    for codon in list(codon_to_aa.keys()): # use dictionary for 64 codons
+    for codon in list(c_to_aa.keys()): # use dictionary for 64 codons
         codon_count[codon]=0  # empty vector to accumulate codon count
 
     for c in list(codon_count.keys()):
-        codon_count[c]+= codons.count(c)
+        codon_count[c]+= segments.count(c)
 
-    df_codcount=pd.DataFrame(list(codon_count.items()) ) #output as dataframe
-    df_codcount.columns=['Codon', 'Obs_Freq'] # column names
-    df_codcount['Amino_Acid'] = [codon_to_aa[codon] for codon in df_codcount['Codon'].values] ## add amino acid column
+    df_codon_count=pd.DataFrame(list(codon_count.items()) ) #output as dataframe
+    df_codon_count.columns=['Codon', 'Obs_Freq'] # column names
+    df_codon_count['Amino_Acid'] = [c_to_aa[codon] for codon in df_codon_count['Codon'].values] ## add amino acid column
 
-    return df_codcount
-
-Count_check = counter(seg_check)
-print(Count_check)
-
+    return df_codon_count
 
 #### caluculate codon usage from codon counts
 
-def calculate_codon_usage(df_codcount): #pass in codon count from previous
-    aa_groups = df_codcount.groupby('Amino_Acid') # by amino acid column in dataframe
-    aa =  df_codcount['Amino_Acid'].unique()  #make a list of all amino acids to iterate over
+def codon_usage(df_codon_count): #pass in codon count from previous
+    aa_groups = df_codon_count.groupby('Amino_Acid') # by amino acid column in dataframe
+    aa =  df_codon_count['Amino_Acid'].unique()  #make a list of all amino acids to iterate over
     df_list = []
     for a in aa:
         d=aa_groups.get_group(a)
@@ -137,55 +121,95 @@ def calculate_codon_usage(df_codcount): #pass in codon count from previous
 
     return pd.concat(df_list) # joins lists
 
-#check
-Usage = calculate_codon_usage(Count_check)
-pd.set_option('display.max_columns', None)
-print(Usage)
-
 
 
 #### Run programmes sequentially, putting output of previous as input
-coding_seqs = get_coding_seqs(Species_cds)
-codon_freq_check = acquire_codon_freq(coding_seqs)
-codon_usage = calculate_codon_usage(codon_freq_check)
-
-##saves final table as csv
-
-codon_usage.to_csv('Species_codon_usage.csv')
-
-
-
-#### need to pull out list of top codon for each amino acid - already been marked in csv 'Species_codon_usage'
+truncation = get_coding_seqs(Species_cds)
+mRNA_conversion = DNA_to_mRNA(truncation)
+segmentation = codon_segmentation(mRNA_conversion)
+codon_counter = counter(segmentation)
+codon_bias = codon_usage(codon_counter)
+pd.set_option('display.max_columns', None) # allows all columns to be displayed
+print(codon_bias)
 
 
+##saves final table as csv (if required)
+#codon_bias.to_csv('Species_codon_usage.csv')
 
 
 
-
-
-
+#### PART 2
+# optimizing protein sequence according to species codon usage bias
 
 
 # import protein sequence for desired optimization. Here eGFP and mCherry are imported.
-#eGFP = open
-#mCherry = open
+protein_cds = open('file path here')
+
+# run previous function to parse sequence, convert to mRNA and segment
+protein_truncation = get_coding_seqs(protein_cds)
+protein_mRNA_conversion = DNA_to_mRNA(protein_truncation)
+protein_segmentation = codon_segmentation(protein_mRNA_conversion)
 
 
+# convert sequence to amino acid sequence using dictionary
+
+# function that converts codons - NOT YET TESTED
+def amino_acid_conversion(c_to_aa, seg_protein_seq):
+    aa = []
+
+    for protein_codons in seg_protein_seq:
+        amino_acid = c_to_aa[protein_codons]
+        aa.append(amino_acid)
+    
+    aa = "".join(aa)
+    
+    return aa
+
+protein_aa = amino_acid_conversion(protein_segmentation)
+# check
+print(protein_aa)
 
 
+#### need to pull out list of top codon for each amino acid - already been marked in csv 'Species_codon_usage'
+# delete all calculation columns are rows where optimal = FALSE 
 
-### parse cds : remove '>'
+df = codon_bias # create new table
+del df['Obs_Freq']
+del df['Relative_codon_usage']
+del df['Relative_adaptive_weights']
+   
+#check     
+print(df)
+
+# filter rows for optimal codon
+df = df[df.optimal != False]
+
+#remove optimal column as no longer required
+del df['optimal']
+
+# create dictionary converting amino acids to codons 
+Top_codons = dict(df.values)
+print(Top_codons)
 
 
+# function to convert aa back to optimal codons
+
+def codon_conversion(Top_codons, protein_aa):
+    new_codons = []
+
+    for amino_acid in protein_aa:
+        new_codon = Top_codons[protein_aa]
+        new_codons.append(new_codon)
+    
+    new_codons= "".join(new_codons)
+    
+    return new_codons
 
 
-### identify codons in sequence
-
-
-
-
-### replace function to put in top codon
-
+# run function
+protein_new_cds = codon_conversion(protein_aa)
+#check
+print(protein_new_cds)
 
 
 
