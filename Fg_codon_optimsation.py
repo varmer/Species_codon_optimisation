2b3deeeae39c3c848faf9894d286e94ea1abbe1f
varmer <75427804+varmer@users.noreply.github.com>
2021-03-07 12:01:19 +0000

    Add files via upload

--- Fg_codon_optimsation.py
+++ Fg_codon_optimsation.py
@@ -60,18 +60,17 @@ def codon_segmentation(sequences):
         return segments
 
 # create a dictionary of amino acid codons
-##break up 6-codon family into 2 and 4 fold (Ser (S), L(Leu), R (Arg))
 c_to_aa = {
     "UUU":"Phe", "UUC":"Phe",         
-    "UCU":"Ser4", "UCC":"Ser4", "UCA":"Ser4", "UCG":"Ser4",
-    "AGU":"Ser2", "AGC":"Ser2",
-    "CUU":"Leu4", "CUC":"Leu4", "CUA":"Leu4", "CUG":"Leu4",
-    "UUA":"Leu2", "UUG":"Leu2",
+    "UCU":"Ser", "UCC":"Ser", "UCA":"Ser", "UCG":"Ser",
+    "AGU":"Ser", "AGC":"Ser",
+    "CUU":"Leu", "CUC":"Leu", "CUA":"Leu", "CUG":"Leu",
+    "UUA":"Leu", "UUG":"Leu",
     
     "UAU":"Tyr", "UAC":"Tyr", "UAA":"STOP", "UAG":"STOP",
     "UGU":"Cys", "UGC":"Cys", "UGA":"STOP", "UGG":"Trp",
-    "CGU":"Arg4", "CGC":"Arg4", "CGA":"Arg4", "CGG":"Arg4",
-    "AGA":"Arg2", "AGG":"Arg2",
+    "CGU":"Arg", "CGC":"Arg", "CGA":"Arg", "CGG":"Arg",
+    "AGA":"Arg", "AGG":"Arg",
     "CCU":"Pro", "CCC":"Pro", "CCA":"Pro", "CCG":"Pro",
     "CAU":"His", "CAC":"His", "CAA":"Gln", "CAG":"Gln",
     
@@ -142,30 +141,50 @@ print(codon_bias)
 # optimizing protein sequence according to species codon usage bias
 
 
-# import protein sequence for desired optimization. Here eGFP and mCherry are imported.
-protein_cds = open('file path here')
+# import protein sequence for desired optimization. Here eGFP is imported.
+protein_cds =  open(r"C:\Users\armerv\Desktop\BLAST\Protein_seqs\eGFP.fasta")
+#protein_cds = protein_cds.read()
 
 # run previous function to parse sequence, convert to mRNA and segment
 protein_truncation = get_coding_seqs(protein_cds)
-protein_mRNA_conversion = DNA_to_mRNA(protein_truncation)
-protein_segmentation = codon_segmentation(protein_mRNA_conversion)
+
+# split string into single aa
+
+def single_aa_segmentation(protein_truncation):
+        n = 1
+        segments = []
+        for seq in protein_truncation:
+            for i in range(0, len(seq), n):
+                segments.append(seq[i:i+n])
+        return segments
+
+# run
+protein_segmentation = single_aa_segmentation(protein_truncation)
+# check
+print(protein_segmentation)
+# convert single letter amini acid sequence to 3 letter aa sequence
+
+aa_single_letter = {
+    "A":"Ala", "C":"Cys", "D": "Asp", "E":"Glu", "F":"Phe", "G":"Gly", "H":"His", "I":"Ile", 
+    "K":"Lys", "L":"Leu", "M":"Met", "N":"Asn", "P":"Pro", "Q":"Gln", "R":"Arg", "S":"Ser",
+    "T":"Thr", "V":"Val", "W":"Trp", "Y":"Tyr"
+}
 
 
 # convert sequence to amino acid sequence using dictionary
 
 # function that converts codons - NOT YET TESTED
-def amino_acid_conversion(c_to_aa, seg_protein_seq):
+def amino_acid_conversion(aa_single_letter, protein_segmentation):
     aa = []
 
-    for protein_codons in seg_protein_seq:
-        amino_acid = c_to_aa[protein_codons]
+    for single_aa in protein_segmentation:
+        amino_acid = aa_single_letter[single_aa]
         aa.append(amino_acid)
     
-    aa = "".join(aa)
     
     return aa
 
-protein_aa = amino_acid_conversion(protein_segmentation)
+protein_aa = amino_acid_conversion(aa_single_letter, protein_segmentation)
 # check
 print(protein_aa)
 
@@ -177,17 +196,18 @@ df = codon_bias # create new table
 del df['Obs_Freq']
 del df['Relative_codon_usage']
 del df['Relative_adaptive_weights']
-   
-#check     
-print(df)
 
 # filter rows for optimal codon
 df = df[df.optimal != False]
+print(df)
 
 #remove optimal column as no longer required
 del df['optimal']
 
 # create dictionary converting amino acids to codons 
+# reorder columns so dictionary is right way
+df = df[["Amino_Acid", "Codon"]]
+
 Top_codons = dict(df.values)
 print(Top_codons)
 
@@ -198,7 +218,7 @@ def codon_conversion(Top_codons, protein_aa):
     new_codons = []
 
     for amino_acid in protein_aa:
-        new_codon = Top_codons[protein_aa]
+        new_codon = Top_codons[amino_acid]
         new_codons.append(new_codon)
     
     new_codons= "".join(new_codons)
@@ -207,16 +227,34 @@ def codon_conversion(Top_codons, protein_aa):
 
 
 # run function
-protein_new_cds = codon_conversion(protein_aa)
+protein_new_cds = codon_conversion(Top_codons, protein_aa)
 #check
 print(protein_new_cds)
 
+protein_new_cds_1 = single_aa_segmentation(protein_new_cds)
 
+# check 
+print(protein_new_cds_1)
 
-### output sequence, saved as fasta file
-
-
+# convert back to DNA
+def mRNA_to_DNA(protein_new_cds_1):
+    new_DNA = []
     
+    for cds in protein_new_cds_1:
+        new_DNA.append(cds.upper().replace('U', 'T'))
+        
+    new_DNA = "".join(new_DNA)
+    
+    return new_DNA
+
+
+optimized_protein = mRNA_to_DNA(protein_new_cds_1)
+
+# check
+print(optimized_protein)
+
+
+
 
 ### sequence comparison i.e. print differences
     
